ae4abee5fdaffb75d0dfc5da2fec3af6964467ce
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-06 11:34:37 +0900

    Add files via upload

--- ex01/first.cpp
+++ ex01/first.cpp
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int parity(unsigned long x){
+  int count=0;
+  while(x!=0){
+    if(x%2==1){
+      count++;
+      x=(x-(x%2))/2;
+    }
+    else if(x%2==0){
+      x=(x-(x%2))/2;
+    }
+  }
 
+  if(count%2==1)
+    return 0;
+  else
+    return 1;
+}
+
+int main(){
+  unsigned long x;
+  scanf("%lo",&x);
+  printf("%d\n",parity(x));
+  return 0;
+}
