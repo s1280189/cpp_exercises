973bd7da37767951cc114d1bdc98694dcf334b7e
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-08 00:19:10 +0900

    Add files via upload

--- ex01/question2.cpp
+++ ex01/question2.cpp
@@ -17,7 +17,6 @@ unsigned long add(unsigned long x, unsigned long y){
 
 unsigned long multiply(unsigned long x, unsigned long y){
   unsigned long result=0;
-  unsigned long reg=1;
   while(y>0){
     if(y&1){
       result=add(result,x);
@@ -25,7 +24,6 @@ unsigned long multiply(unsigned long x, unsigned long y){
     x=x<<1;
     y=y>>1;
   }
-  
   return result;
 }
 
