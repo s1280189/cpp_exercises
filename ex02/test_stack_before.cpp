205fcd645a3a42827467f9bc82da6c70e36ac55f
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-14 22:48:00 +0900

    Create test_stack_before.cpp

---
+++ ex02/test_stack_before.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include "stack.h"
+
+int main(){
+  //COMPLETE
+  //Write tests for testing your stack implementetaion
+  Stack::pop();
+  std::cout<<(Stack::size())<<std::endl;
+  std::cout<<(Stack::empty())<<std::endl;
+
+  
+  Point point;
+  point.x=1;
+  point.y=1;
+  Stack::push(point);
+  std::cout<<(Stack::empty())<<std::endl;
+  point=top();
+  printf("%d, %d\n",point.x,point.y);
+  Stack::pop();
+
+  
+  point.x=1;
+  point.y=2;
+  Stack::push(point);
+
+  point.x=3;
+  point.y=3;
+  Stack::push(point);
+  Stack::push(point);
+  Stack::push(point);
+  std::cout<<(Stack::size())<<std::endl;
+
+  Stack::pop();
+  Stack::pop();
+  Stack::pop();
+  Stack::pop();
+
+  int i;
+  point.x=4;
+  point.y=4;
+  for(i=0;i<_max_size;i++){
+    Stack::push(point);
+  }
+
+  std::cout<<(size())<<std::endl;
+  std::cout<<(full())<<std::endl;
+  Stack::push(point);
+  std::cout<<full()<<std::endl;
+  Stack::push(point);
+  return 0;
+}
