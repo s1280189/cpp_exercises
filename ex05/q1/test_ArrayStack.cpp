0473d07ba72704db199b897eaca9e9e73feff63b
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-20 11:29:15 +0900

    Add files via upload

---
+++ ex05/q1/test_ArrayStack.cpp
@@ -0,0 +1,17 @@
+#include "ArrayStack.h"
+
+int main(){
+  ArrayStack stack1;
+  stack1.push('1');
+  stack1.push('2');
+  stack1.push('3');
+
+  ArrayStack stack2(5);
+  stack2.push('1');
+  stack2.push('2');
+  stack2.push('3');
+  stack2.push('4');
+
+
+  return 0;
+}
