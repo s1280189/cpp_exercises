feab2bf2236e9e186438d3b73d5e534a025e5042
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-20 12:22:13 +0900

    Add files via upload

--- ex05/q1/test_ArrayStack.cpp
+++ ex05/q1/test_ArrayStack.cpp
@@ -2,15 +2,15 @@
 
 int main(){
   ArrayStack stack1;
-  stack1.push('1');
-  stack1.push('2');
-  stack1.push('3');
+  stack1.push("1");
+  stack1.push("2");
+  stack1.push("3");
 
   ArrayStack stack2(5);
-  stack2.push('1');
-  stack2.push('2');
-  stack2.push('3');
-  stack2.push('4');
+  stack2.push("1");
+  stack2.push("2");
+  stack2.push("3");
+  stack2.push("4");
 
 
   return 0;
