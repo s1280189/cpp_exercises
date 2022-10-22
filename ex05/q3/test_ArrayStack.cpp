97907a05e351fce00845439c4458a31776f60f13
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-22 23:04:56 +0900

    Add files via upload

--- ex05/q3/test_ArrayStack.cpp
+++ ex05/q3/test_ArrayStack.cpp
@@ -21,33 +21,11 @@ int main(){
   ArrayStack stack6(10);
   stack6=stack6;
 
-
-  max=stack1.size();
-  for(int i=0;i<=max;i++){
-    std::cout<<"stack1 =:" << stack1.top() <<std::endl;
-    std::cout<<"stack3 =:" << stack3.top() <<std::endl;
-    std::cout<<"stack5 =:" << stack5.top()  << std::endl;
-    stack1.pop();
-    stack3.pop();
-    stack5.pop();
-  }
-
-  std::cout<<"------------------------"<<std::endl;
-  
-  max=stack2.size();
-   for(int i=0;i<=max;i++){
-     std::cout<<"stack2 =:" << stack2.top() <<std::endl;
-     std::cout<< "stack4 =:" << stack4.top() << std::endl;
-    stack4.pop();
-    stack2.pop();
-  }
-
    stack6.push(1);
    stack6.push(2);
    stack6.push(3);
    stack6.push(4);
    stack6.push(5);
-
 
    ArrayStack stack7 = std::move(stack5);
 
@@ -60,6 +38,10 @@ int main(){
       stack5.pop();
       stack7.pop();
     }
+
+    std::cout<<"---------------"<<std::endl;
+
+    
 
     max=stack6.size;
     for(int i=0;i<max;i++){
