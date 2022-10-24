07918b6e36861fcb6342cdda36b87972c6237275
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-24 12:27:53 +0900

    Add files via upload

---
+++ ex6/q2/rectangle.cpp
@@ -0,0 +1,32 @@
+#include<rectangle.h>
+
+Rectangle::Rectangle(){
+  left_corner.x=0;
+  left_corner.y=0;
+  width=0;
+  height=0;
+};
+
+Rectangle::Rectangle(Point p1, float p1_width, float p1_height){
+   left_corner = p1;
+   width = p1_width;
+   height = p1_height;
+};
+
+virtual std::string Ractangle::get_name() const{
+  return "Restangle";
+}
+
+virtual float Rectangle::compute_area() const{
+  return width*height;
+}
+
+virtual Shape* Rectangle::create() const {
+  return new Rectangle();
+}
+
+virtual Shape* Rectangle::clone() const{
+  return new Rectangle();
+}
+
+virtual ~Shape();
