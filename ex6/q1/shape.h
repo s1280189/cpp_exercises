82b3a974dca7953412f6b0f57de278b33d7a52ea
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-26 16:45:14 +0900

    Add files via upload

--- ex6/q1/shape.h
+++ ex6/q1/shape.h
@@ -6,16 +6,16 @@
 
 class Shape{
  public:
-  virtual std::string get_name() const=0;
+  virtual std::string get_name() const{};
 
-  virtual float compute_area() const=0;
+  virtual float compute_area() const{};
 
   
-  virtual Shape* create() const=0;
+  virtual Shape* create() const{};
 
-  virtual Shape* clone() const=0;
+  virtual Shape* clone() const{};
 
-  virtual ~Shape() =0;
+  virtual ~Shape() {};
 
 }
 
