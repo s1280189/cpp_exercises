9e4a6d162d4b4932a6bbf9043d73a6e5aeb94cdb
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-26 23:05:56 +0900

    Add files via upload

--- ex6/q2/test_shape.cpp
+++ ex6/q2/test_shape.cpp
@@ -1,13 +1,59 @@
 #include "rectangle.cpp"
 #include "disk.cpp"
 
+struct Point{
+  float x;
+  float y;
+};
+
+
 int main(){
-  Rectangle rec1;
-  Rectangle rec2(rec1);
+  Point p1;
+  p1.x=2;
+  p1.y=2;
+  
+  Rectangle rec1(p1, 3, 4);
+  Rectangle rec2;
+  Rectangle rec3;
+
+  rec2=rec1.clone();
+  rec3=rec1.create();
+
+  std::cout<< rec1.get_name() << std::endl;
+  std::cout<< "recatngle1 area ->" << rec1.compute_area() <<std::endl;
+
+  std::cout<<"rectangle2 -> rectangle1's clone"<<std::endl;
+  std::cout<< "rectangle2 check get_name ->" << rec2.get_name() <<std::endl;
+  std::cout<<"recatngle2 check compute_area()->"
+	   <<rec2.compute_area()<<std::endl;
+
+  std::cout<<"rectangle3 check get_name->"
+	   << rec3.get_name() << std::endl;
+  std::cout<<"recatnagle3 check compute_area()"
+	   <<rec3.compute_area()<<std::endl;
+  
 
 
-  Disk disk1;
-  Disk disk2(disk1);
+  Disk disk1(p1, 5);
+  Disk disk2, disk3;
+  disk2=disk1.clone;
+  disk3=disk1.create;
+  
+
+  std::cout<< disk1.get_name() << std::endl;
+  std::cout<< "disk1 area ->" << disk1.compute_area() <<std::endl;
+
+  std::cout<<"disk2 -> disk1's clone"<<std::endl;
+  std::cout<< "disk2 check get_name ->" << disk2.get_name() <<std::endl;
+  std::cout<<"disk2 check compute_area()->"
+           <<disk2.compute_area()<<std::endl;
+
+  std::cout<<"disk3 -> new create disk1"<<std::endl;
+  std::cout<<"disk3 check get_name->"
+           << disk3.get_name() << std::endl;
+  std::cout<<"recatnagle3 check compute_area()"
+           <<disk3.compute_area()<<std::endl;
+
 
   return 0;
 }
