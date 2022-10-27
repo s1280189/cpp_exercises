4c917bdfb41101c2df9db5a3dd07544bea523f5d
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-27 18:56:00 +0900

    Add files via upload

--- ex6/q2/disk.cpp
+++ ex6/q2/disk.cpp
@@ -6,27 +6,27 @@ Disk::Disk(){
   center.x=0;
   center.y=0;
   radius=0;
-};
+}
 
 Disk::Disk(Point cent1, float rad1){
   center=cent1;
   radius=rad1;
-};
+}
 
-virtual std::string Disk::get_name() const{
+std::string Disk::get_name() const{
   return "Circle";
-};
+}
 
-virtual float Disk::compute_area() const{
+float Disk::compute_area() const{
   return radius*radius*PI;
 }
 
-virtual Shape* Disk::create() const{
+Disk* Disk::create() const{
   return new Disk();
 }
 
-virtual Shape* Disk::clone() const{
+Disk* Disk::clone() const{
   return new Disk();
 }
 
-virtual ~Shape();
+Disk::~Disk();
--- ex6/q2/disk.h
+++ ex6/q2/disk.h
@@ -25,4 +25,5 @@ class Disk: public Shape{
 
 
 };
-  
+
+#endif
