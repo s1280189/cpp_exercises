2ac97d5c8b3dfb9fe66d06294d827f4bc6a7ca57
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-28 18:56:36 +0900

    Add files via upload

---
+++ ex6/q3/polygon.cpp
@@ -0,0 +1,44 @@
+#include "polygon.h"
+
+Polygon::Polygon(){
+  n=0;
+  p=nullptr;
+}
+
+Polygon::Polygon(Point* p1, int n1){
+  n=n1;
+  delete[] p;
+  p=new float[n];
+  for(int i=0;i<n;i++){
+    p[i]=p1[i];
+  }
+    
+}
+
+std::string Polygon::get_name() const{
+  return "Polygon";
+}
+
+float Polygon::compute_area() const{
+  float result=0;
+  for(int i=0;i<n-1;i++){
+    result=result+(p[i].x*p[i+1].y - p[i].y*p[i+1].x);
+  }
+  
+  return result/2;
+}
+
+
+Polygon* Polygon::create() const{
+  return new Polygon();
+}
+
+Polygon* Polygon::clone() const{
+  return new Polygon(*this);
+}
+
+Polygon::~Polygon(){};
+
+
+
+  
