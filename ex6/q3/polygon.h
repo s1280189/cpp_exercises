6542d11c267c4ede9f42187670b80a88eba50e2d
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-28 18:02:26 +0900

    Add files via upload

---
+++ ex6/q3/polygon.h
@@ -0,0 +1,26 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+#include "shape.h"
+
+
+
+class Polygon: public Shape{
+
+ private:
+  Point*  p;
+  int n;
+  
+ public:
+  Polygon();
+  Polygon(Point* p, int n);
+
+  virtual std::string get_name() const;
+  virtual float compute_area() const;
+  virtual Polygon* create() const;
+  virtual Polygon* clone() const;
+
+  virtual ~Polygon();
+  
+
+};
+#endif
