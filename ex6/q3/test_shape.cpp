fc87999bd759038866e9a24cdaf9db4321a045d9
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-29 21:30:31 +0900

    Add files via upload

--- ex6/q3/test_shape.cpp
+++ ex6/q3/test_shape.cpp
@@ -1,5 +1,6 @@
 #include "rectangle.cpp"
 #include "disk.cpp"
+#include "polygon.cpp"
 
 struct Point{
   float x;
@@ -8,53 +9,37 @@ struct Point{
 
 
 int main(){
-  Point p1;
+  int n=5;
+  Polygon p1[5];
+  p1.x=1;
+  p1.y=1;
+
   p1.x=2;
   p1.y=2;
+
+  p1.x=3;
+  p1.y=3;
+
+  p1.x=4;
+  p1.y=4;
+
+  p1.x=5;
+  p1.y=5;
   
-  Rectangle rec1(p1, 3, 4);
-  Rectangle* rec2;
-  Rectangle* rec3;
+  Polygon p1(p1, 5);
+  Polygon* p2;
+  Polygon* p3;
 
-  rec2=rec1.clone();
-  rec3=rec1.create();
+  p2=p1.clone();
+  p3=p1.create();
 
-  std::cout<< rec1.get_name() << std::endl;
-  std::cout<< "recatngle1 area ->" << rec1.compute_area() <<std::endl;
-
-  std::cout<<"rectangle2 -> rectangle1's clone"<<std::endl;
-  std::cout<< "rectangle2 check get_name ->" << rec2.get_name() <<std::endl;
-  std::cout<<"recatngle2 check compute_area()->"
-	   <<rec2.compute_area()<<std::endl;
-
-  std::cout<<"rectangle3 check get_name->"
-	   << rec3.get_name() << std::endl;
-  std::cout<<"recatnagle3 check compute_area()"
-	   <<rec3.compute_area()<<std::endl;
-  
-
-
-  Disk disk1(p1, 5);
-  Disk* disk2;
-  Disk* disk3;
-  disk2=disk1.clone;
-  disk3=disk1.create;
-  
-
-  std::cout<< disk1.get_name() << std::endl;
-  std::cout<< "disk1 area ->" << disk1.compute_area() <<std::endl;
-
-  std::cout<<"disk2 -> disk1's clone"<<std::endl;
-  std::cout<< "disk2 check get_name ->" << disk2.get_name() <<std::endl;
-  std::cout<<"disk2 check compute_area()->"
-           <<disk2.compute_area()<<std::endl;
-
-  std::cout<<"disk3 -> new create disk1"<<std::endl;
-  std::cout<<"disk3 check get_name->"
-           << disk3.get_name() << std::endl;
-  std::cout<<"recatnagle3 check compute_area()"
-           <<disk3.compute_area()<<std::endl;
-
+  std::cout<< p1.get_name() << std::endl;
+  std::cout<< "polygon1 are:"<<p1.compute_area() <<std::endl;
+  std::cout<< "polygon2(clone):getname: "<<p2.get_name()<< std::endl;
+  std::cout<< "polygon2(clone):area: " << p2.compute_area()<<std::endl;
+  std::cout<< "polygon3(create):getname: "<<p3.get_name()<< std::endl;
+  std::cout<< "polygon3(create):area: " << p3.compute_area()<<std::endl;
+		       
 
   return 0;
 }
