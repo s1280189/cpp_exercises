871c13ab9edc1aaad69a3124653e411438006d2f
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-29 21:32:21 +0900

    Update test_shape.cpp

--- ex6/q3/test_shape.cpp
+++ ex6/q3/test_shape.cpp
@@ -26,19 +26,19 @@ int main(){
   p1.x=5;
   p1.y=5;
   
-  Polygon p1(p1, 5);
+  Polygon po1(p1, 5);
   Polygon* p2;
   Polygon* p3;
 
-  p2=p1.clone();
-  p3=p1.create();
+  pol2=p1.clone();
+  pol3=p1.create();
 
-  std::cout<< p1.get_name() << std::endl;
-  std::cout<< "polygon1 are:"<<p1.compute_area() <<std::endl;
-  std::cout<< "polygon2(clone):getname: "<<p2.get_name()<< std::endl;
-  std::cout<< "polygon2(clone):area: " << p2.compute_area()<<std::endl;
-  std::cout<< "polygon3(create):getname: "<<p3.get_name()<< std::endl;
-  std::cout<< "polygon3(create):area: " << p3.compute_area()<<std::endl;
+  std::cout<< pol1.get_name() << std::endl;
+  std::cout<< "polygon1 are:"<<pol1.compute_area() <<std::endl;
+  std::cout<< "polygon2(clone):getname: "<<pol2.get_name()<< std::endl;
+  std::cout<< "polygon2(clone):area: " << pol2.compute_area()<<std::endl;
+  std::cout<< "polygon3(create):getname: "<<pol3.get_name()<< std::endl;
+  std::cout<< "polygon3(create):area: " << pol3.compute_area()<<std::endl;
 		       
 
   return 0;
