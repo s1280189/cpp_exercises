95ae784e8085a9ddb5e58739a87aeb21510b06e5
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-04 21:05:42 +0900

    Update test_ArrayStack.cpp

--- ex7/test_ArrayStack.cpp
+++ ex7/test_ArrayStack.cpp
@@ -12,13 +12,26 @@ int main(){
   stack1.push("1");
   stack1.push("2");
   stack1.push("3");
+  
+   try{
+    stack1.pop();
+    stack1.top();
+  } catch(std::runtime_error& e){
+    std::cout<< e.what() <<std::endl;
+  };
 
 
-  ArrayStack stack2(5);
-  stack2.push("1");
-  stack2.push("2");
-  stack2.push("3");
-  stack2.push("4");
+  stack1.pop();
+  stack1.pop();
+  stack1.pop();
+  
+   try{
+    stack1.pop();
+    stack1.top();
+  } catch(std::runtime_error& e){
+    std::cout<< e.what() <<std::endl;
+  };
+
 
 
   return 0;
