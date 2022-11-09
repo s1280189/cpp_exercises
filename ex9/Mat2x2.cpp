b3cc84ce096640d9abacf2b31698662e4f7e29ea
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-10 08:56:51 +0900

    Update Mat2x2.cpp

--- ex9/Mat2x2.cpp
+++ ex9/Mat2x2.cpp
@@ -13,11 +13,11 @@ Mat2x2<T,N>::Mat2x2(){
 }
 
 Mat2x2<T,N>::Mat2x2(T x[2*N]){
-  for(int i=0;i<N;i++){
-	for(int j=0;j<N;j++){
-		e[i][j]=x[i][j];
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			e[i][j]=x[i*n+j];
+		}
 	}
-  }
 }
 
 
