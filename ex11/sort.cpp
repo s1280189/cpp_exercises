4eff2d4a22e97b780854edd2f5b795c6dff45ffb
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-14 11:40:26 +0900

    Add files via upload

---
+++ ex11/sort.cpp
@@ -0,0 +1,20 @@
+#include "sort.h"
+#include <list>
+#include <algorithm>
+
+using namespace PL4;
+int main(){
+  std::list<int>::iterator t;
+
+  t.push_back(1);
+  t.push_back(5);
+  t.push_back(3);
+  t.push_back(2);
+  t.push_back(9);
+  t.push_back(6);
+
+  sort(t.cbegin(), t.cend());
+
+
+  return 0;
+}
---
+++ ex11/sort.h
@@ -0,0 +1,26 @@
+#ifndef SORT_H
+#define SORT_H
+#include <iostream>
+#include <cmath>
+#include <cctype>
+
+namespace PL4 {
+  template<class BiDirlt> void sort(BiDirlt begin, BiDirlt end){
+    BiDirlt i;
+    BiDirlt j;
+    BiDirlt temp;
+
+    for(i=begin;i<=end;i++){
+      for(j=begin++;j<=end;j++){
+	if(i > j)
+	  {
+	    temp = i;
+	    i = j;
+	    j = temp;
+	  }
+      }
+    }
+
+  };
+
+#endif
