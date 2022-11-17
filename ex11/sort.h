523c2881ef35d84a8fdbf8d7ba6cb18f9c6c4047
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-17 22:51:33 +0900

    Update sort.h

--- ex11/sort.h
+++ ex11/sort.h
@@ -6,21 +6,10 @@
 
 namespace PL4 {
   template<class BiDirlt> void sort(BiDirlt begin, BiDirlt end){
-    BiDirlt i;
+    BiDirlt i=1;
     BiDirlt j;
-    BiDirlt temp;
-
-    for(i=begin;i<end;i++){
-      for(j=begin++;j<end;j++){
-	if(i > j)
-	  {
-	    temp = i;
-	    i = j;
-	    j = temp;
-	  }
-      }
-    }
-
+    BiDirlt tem
+	  
   };
 }
 #endif
