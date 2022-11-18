43d63cd0fd9dd9aa21da30eb1c6eca48e25fb970
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-18 22:25:13 +0900

    Create ex12_2.h

---
+++ ex12/ex12_2.h
@@ -0,0 +1,60 @@
+#ifndef EX12_2_H
+#define Ex12_2_H
+#include<iostream>
+#include<sstream>
+#include<cmath>
+#include<list>
+#include<queue>
+
+struct Point{
+Point(): x(0.0), y(0.0), z(0.0) {}
+Point(double x,double y,double z):x(x), y(y), z(z) {}
+  double x,y,z;
+};
+
+class Compare {
+ public:
+  bool operator()(const Point fir, const Point sec){
+    if( sqrt((fir.x * fir.x )+(fir.y*fir.y)+(fir.z*fir.z))
+	< sqrt((sec.x*sec.x)+(sec.y*sec.y)+(sec.z*sec.z)))
+      return true;
+
+    else
+      return false;
+  }
+}
+
+
+
+std::list<Point> find_k_closest(int k, std::list<Point> point){
+  
+  std::priority_queue<
+    Point,
+    std::vector<Point>,
+    Compare
+    > queue;
+  
+  std::list<Point> ls;
+
+  while(!point.empty()){
+    queue.push(point.front());
+    point.pop_front();
+  }
+
+
+  for(int i=0;i<queue.size()-k;i++){
+    queue.pop();
+  }
+
+  for(int i=0;i<k;i++){
+    ls.push_front(queue.top());
+    queue.pop();
+  }
+  
+  return ls;
+}
+    
+    
+
+
+#endif
