4cf61d8e5f585893f0999bcfa47db6a945ec1271
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-21 22:26:29 +0900

    Update test_median.cpp

--- ex13/q1/test_median.cpp
+++ ex13/q1/test_median.cpp
@@ -2,10 +2,10 @@
 
 
 int main(){
-  std::vector<float> v1{5, 10, 6, 4, 3, 2, 6, 7, 9, 3};
+  std::vector<float> v1{4, 2, 3, 1};
   std::cout<< " median1 is ..." << compute_median(v1) << std::endl;
 
-  std::vector<float> v2{9,8,7,6,5,4,3};
+  std::vector<float> v2{5, 4, 2, 1, 3};
   std::cout<< "median2 is..." << compute_median(v2) << std::endl;
 
   return 0;
