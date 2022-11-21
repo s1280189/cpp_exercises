e53c006c6d99f39a802a209112e6781dfbaa0c41
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-21 18:40:14 +0900

    Update sort.h

--- ex11/sort.h
+++ ex11/sort.h
@@ -7,20 +7,21 @@
 
 namespace PL4 {
   template<class BiDirlt> void sort(BiDirlt begin, BiDirlt end){
-    auto i,j,x;
-	  
-	  i=begin;
-	  while(i != end){
-		  j=*i;
-		  x=j--;
-		  while(j != 0 && j > x ){
-			  
-			  swap(j, x);
-			  j--;
-			  x--;
-		  } 
-	  i++;
-  };
+      auto i=begin;
+      while(i!=end){
+	     auto j=i;
+	     while(j!=end){
+	         auto x=*j-1;
+	         auto y=*j;
+	         while(x > y){
+	             auto temp=x;
+	             x=y;
+	             y=temp;
+	         }
+	     j--;
+	     }
+	      i++;
+      }
 };
 }
 #endif
