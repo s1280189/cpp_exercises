c0373038845e319a85337a23dfbbded0dda7adeb
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-22 20:41:10 +0900

    Update test_closest.cpp

--- ex13/q2/test_closest.cpp
+++ ex13/q2/test_closest.cpp
@@ -3,7 +3,7 @@
 
 int main(){
   std::vector<float> v1{4, 2, 3, 1, 5, 9 , 8};
-  std::vector<float> vr=compute_k_closest(v1);
+  std::vector<float> vr=compute_k_closest(3, v1);
   
   for(int i=0; i<vr.size(); i++){
     std::cout<< vr[i] <<" " <<std::endl;
