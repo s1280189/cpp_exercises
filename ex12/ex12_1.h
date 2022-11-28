bdd7d67cb5d32c33442fb77645e26277fc0b2470
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-11-28 23:50:33 +0900

    Update ex12_1.h

--- ex12/ex12_1.h
+++ ex12/ex12_1.h
@@ -44,14 +44,12 @@ std::list<Point> find_k_closest(int k, std::list<Point> point){
   }
 
 
-  while(!queue.empty()){
+  for(int i=0;i<k;i++){
     ls.push_front(queue.top());
     queue.pop();
   }
 
-  for(int i=0;i<ls.size()-(k-1);i++){
-    ls.pop_back();
-  }
+  
   
   return ls;
 }
